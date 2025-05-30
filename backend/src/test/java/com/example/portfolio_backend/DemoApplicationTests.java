f360be5e5b3ea354d9923b35434e4732a2468e87
soma-desktop-pc <somahiranodev@gmail.com>
2025-05-31 00:23:23 +0900

    Set SpringBootTest webEnvironment to NONE for DemoApplicationTests

--- backend/src/test/java/com/example/portfolio_backend/DemoApplicationTests.java
+++ backend/src/test/java/com/example/portfolio_backend/DemoApplicationTests.java
@@ -2,9 +2,10 @@ package com.example.portfolio_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @ActiveProfiles("test")
 class DemoApplicationTests {
 
